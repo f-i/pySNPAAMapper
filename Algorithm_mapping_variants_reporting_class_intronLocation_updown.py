88eb776e6676a5a0cfb7597faba47ca52ffdc825
f-i <cfu3@student.kent.edu>
2022-05-24 14:35:51 -0400

    added a faster 3rd Python script; keeping the slow one (suffix with .py_slow) for future study

--- Algorithm_mapping_variants_reporting_class_intronLocation_updown.py
+++ Algorithm_mapping_variants_reporting_class_intronLocation_updown.py
@@ -1,131 +1,124 @@
-#!/usr/bin/env python
-'''
-Algorithm for mapping identified variants onto the genomic location and
-reporting the hit class
+''' algorithm for mapping identified variants onto the genomic location and reporting the hit class
 
-USAGE:
-    python Algorithm_mapping_variants_reports_class_intronLocation.py ChrAll_knownGene.txt.exons VCF_input_file_in_tab_delimited_format.txt IntronExon_boundary_in_bp
+python3 Algorithm_mapping_variants_reporting_class_intronLocation_updown.py ChrAll_knownGene.txt.exon VCF_input_file_in_tab_delimited_format.vcf
 '''
 
 import sys
-import pandas as pd
+from sys import argv
+import csv
+
+exon_file = None
+snp_file = None
+exon_buffer = None
+intron_option = None
+
+# increase field limit
+limit = sys.maxsize
+while True:
+    # decrease limit by factor 10 as long as OverflowError occurs
+    try:
+        csv.field_size_limit(limit)
+        break
+    except OverflowError:
+        limit = int(limit/10)
 
 
-def link(who, a):
-    '''who is a string from a list: 'cds', 'intron', 'utr5', 'utr3', 'upstream'
-    and 'downstream'; a is a Pandas DataFrame of 'ChrAll_knownGene.txt.exons' '''
-    start = who + 'Start'
-    end = who + 'End'
-    b = a[((a[start].notnull())|(a[end].notnull()))&(~a.chrom.str.contains('_'))]
-    b.loc[:,start] = b[start].astype(int)
-    b.loc[:,end] = b[end].astype(int)
-    new_col = 'chrom_' + who + 'Start'
-    b.loc[:,new_col] = b.chrom + '_'+b[start].astype(str)
-    d = b.groupby('chrom')[start].apply(list).apply(sorted)
-    e = b.groupby(new_col, sort=False).agg({end:max,'name':'first'})
-    return d,e
-
-inFile = sys.argv[1]  # Stores file one for input checking; note that sys.argv[0] is this script file name
-print("Reading your first data file "+inFile+" ...")
-a = pd.read_csv(inFile, delimiter="\t")
-
-inFile0 = sys.argv[2]
-print("Reading your second data file, the VCF input file in tab delimited format "+inFile+" ...")
-d = pd.read_csv(inFile0, delimiter="\t", header=26)  #header numbers start from 0
-d["HIT"] = ""
-d["gene"] = ""
-
-if len(sys.argv) == 4:
-    intronOpt=1
-    exonbuffer=sys.argv[3]
-    print("The program assumes that you DO want to report how far the variant falls in the exon boundary.\nOnly variants flanking its nearby exon with <= {} bp is reported\n".format(exonbuffer))
-elif len(sys.argv) == 3:
-    intronOpt=-1
-    print("The program assumes that you do NOT want to report how far the variant falls in the exon boundary.\n")
+if(len(argv) == 3):
+    exon_file = argv[1]
+    snp_file = argv[2]
+    print('The program assumes that you do NOT want to report how far the variant falls in the exon boundary.')
+elif(len(argv) == 4):
+    exon_file = argv[1]
+    snp_file = argv[2]
+    exon_buffer = int(argv[3])
+    print('The program assumes that you DO want to report how far the variant falls in the exon boundary.')
+    print('Only variants flanking their nearby exon within <=' + str(exon_buffer) + ' bp are reported')
+    intron_option = True
 else:
-    print("The input commands do not meet the requirement. Please see the README file and try again.\n")
-    exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
+    print('The input commands do not meet the requirement. Please see the README file and try again.')
+    quit()
 
-df=pd.DataFrame()
-for i in range(len(d['POS'])):
-    whos = ['cds', 'intron', 'utr5', 'utr3', 'upstream', 'downstream']
-    for j in whos:
-        b,c = link(j,a)
-        tmp_n=d['POS'][i]
-        tmpchr=d['#CHROM'][i]
-        if tmpchr in b.index:
-            try:
-                ri=b[tmpchr].index(tmp_n)
-            except ValueError:
-                tmp=b[tmpchr][:]  #this is how we clone a list
-                tmp.append(tmp_n)
-                ri=sorted(tmp).index(tmp_n)-1
-        if j=='cds':
-            if tmp_n>b[tmpchr][ri] and \
-            tmp_n<=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End'] and \
-            b[tmpchr][ri]!='NA' and \
-            c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']!='NA':
-                d.loc[i,'HIT']="CDSHIT"
-                d.loc[i,'gene']=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name']
-                df = df.append(d.iloc[[i]])
-                print(d.iloc[[i]].to_string(index=False, header=False),
-                      "CDSHIT",c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name'])
-        if j=='intron':
-            if intronOpt==1:
-                if tmp_n>=b[tmpchr][ri] and \
-                tmp_n<(b[tmpchr][ri]+exonbuffer) or \
-                tmp_n<=(c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1) and \
-                tmp_n>(c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1-exonbuffer) and \
-                b[tmpchr][ri]!='NA' and \
-                c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']!='NA':
-                    if (tmp_n-b[tmpchr][ri])<exonbuffer:
-                        hitbuffer=tmp_n-b[tmpchr][ri]+1
-                        direction="right"
-                    if (c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1-tmp_n)<exonbuffer:
-                        hitbuffer=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1-tmp_n+1
-                        direction="left"
-                    d.loc[i,'HIT']="INTRONHIT."+str(hitbuffer)+"."+direction
-                    d.loc[i,'gene']=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name']
-                    df = df.append(d.iloc[[i]])
-                    print(d.iloc[[i]].to_string(index=False, header=False),
-                          "INTRONHIT."+str(hitbuffer)+"."+direction,
-                          c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name'])
-            if intronOpt==-1:
-                if tmp_n>=b[tmpchr][ri] and \
-                tmp_n<=(c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1) and \
-                b[tmpchr][ri]!='NA' and \
-                c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']!='NA':
-                    d.loc[i,'HIT']="INTRONHIT"
-                    d.loc[i,'gene']=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name']
-                    df = df.append(d.iloc[[i]])
-                    print(d.iloc[[i]].to_string(index=False, header=False),
-                          "INTRONHIT",
-                          c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name'])
-        if j=='utr5' or j=='upstream':
-            if tmp_n>b[tmpchr][ri] and \
-            tmp_n<=(c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']+1) and \
-            b[tmpchr][ri]!='NA' and \
-            c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']!='NA':
-                d.loc[i,'HIT']=j.upper()+"HIT"
-                d.loc[i,'gene']=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name']
-                df = df.append(d.iloc[[i]])
-                print(d.iloc[[i]].to_string(index=False, header=False),
-                      j.upper()+"HIT",c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name'])
-        if j=='utr3' or j=='downstream':
-            if tmp_n>=b[tmpchr][ri] and \
-            tmp_n<=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End'] and \
-            b[tmpchr][ri]!='NA' and \
-            c.loc[tmpchr+'_'+str(b[tmpchr][ri]),j+'End']!='NA':
-                d.loc[i,'HIT']=j.upper()+"HIT"
-                d.loc[i,'gene']=c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name']
-                df = df.append(d.iloc[[i]])
-                print(d.iloc[[i]].to_string(index=False, header=False),
-                      j.upper()+"HIT",c.loc[tmpchr+'_'+str(b[tmpchr][ri]),'name'])
+output_file = snp_file + '.append'
 
-outFile = inFile0 + ".append"
-try:
-    print("Saving data to file "+outFile+" ...")
-    df.to_csv(outFile,index=False,sep='\t',na_rep='NA',float_format='%.9g')
-except IOError:
-    print("Failed to create " + outFile)
-    exit(1)
+class FeatureType:
+    def __init__(self, name, ext):
+        self.name = name
+        self.ext = ext
+        self.chrom = {} # start position -> stop position
+        self.gene = {} # chrom_start -> stop position
+        self.starts = {} # chrom -> start positions
+
+        for line in list(csv.reader(open(exon_file + '.' + ext + '_link_shrink'), delimiter='\t')):
+            self.chrom[line[0]] = int(line[1])
+        for line in list(csv.reader(open(exon_file + '.' + ext + '_gene'), delimiter='\t')):
+            self.gene[line[0]] = line[1]
+
+        # store start positions for binary search
+        for line in list(csv.reader(open(exon_file + '.' + ext), delimiter='\t')):
+            chrom = line[0]
+            arr = []
+            for start in line[1:]:
+                if start != '':
+                    arr.append(int(start))
+            if chrom not in self.starts:
+                self.starts[chrom] = arr
+            else:
+                self.starts.extend(arr)
+
+
+def binary_search(starts, snp_start):
+    '''faster binary search'''
+    lo = 0
+    hi = len(starts) - 1
+    while lo < hi:
+        mid = (lo + hi + 1) // 2
+        if snp_start >= starts[mid]:
+            lo = mid
+        else:
+            hi = mid - 1
+    return lo
+
+feature_types = [FeatureType('CDSHIT', 'cds'), FeatureType('INTRON', 'intron'), FeatureType('UTR5', 'utr5'), FeatureType('UTR3', 'utr3'), FeatureType('UPSTREAM', 'upstream'), FeatureType('DOWNSTREAM', 'downstream')]
+
+# CDS - start is 0-indexed, end is 1-indexed
+# UTR5 - start is 0-indexed, end is 0-indexed
+# UPSTREAM - start is 0-indexed, end is 0-indexed
+# UTR3 - start is 1-indexed, end is 1-indexed
+# DOWNSTREAM - start is 1-indexed, end is 1-indexed
+# INTRON - start is 1-indexed, add buffer because end is 0-indexed
+
+with open(output_file, 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='\t')
+
+    # map vcf variant back onto genome location and report hit class
+    for line in list(csv.reader(open(snp_file), delimiter='\t')):
+        if line[0][0] == '#':
+            continue
+        snp_chr = line[0]
+        if 'chr' not in snp_chr:
+            snp_chr = 'chr' + snp_chr
+        if snp_chr == 'chrMT':
+            snp_chr = 'chrM'
+        snp_start = int(line[1])
+
+        for typ in feature_types:
+            starts = typ.starts[snp_chr]
+            mid = binary_search(starts, snp_start-1)
+            output = line.copy()
+
+            if typ.name == 'INTRON' and intron_option:
+                if snp_start >= starts[mid] and snp_start < starts[mid] + exon_buffer or snp_start <= typ.chrom[snp_chr + '_' + str(starts[mid])] + 1 and snp_start > typ.chrom[snp_chr + '_' + str(starts[mid])] + 1 - exon_buffer:
+                    if snp_start - starts[mid] < exon_buffer:
+                        output.append('INTRONHIT.' + str(snp_start - starts[mid] + 1) + '.right')
+                        output.append(typ.gene[snp_chr + '_' + str(starts[mid])])
+                        writer.writerow(output)
+                    elif feature_types[1].chrom[snp_chr + '_' + str(starts[mid])] + 1 - snp_start < exon_buffer:
+                        output.append('INTRONHIT.' + str(typ.chrom[snp_chr + '_' + str(starts[mid])] - snp_start + 2))
+                        output.append(typ.gene[snp_chr + '_' + str(starts[mid])])
+                        writer.writerow(output)
+            elif snp_start-1 >= starts[mid] and snp_start <= typ.chrom[snp_chr + '_' + str(starts[mid])]:
+                output.append(typ.name)
+                output.append(typ.gene[snp_chr + '_' + str(starts[mid])])
+                writer.writerow(output)
+
+        print("Done for SNP " + snp_chr + "_" + str(snp_start) + ".")
