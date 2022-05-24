e1178265c46821fff90c86e7450f49fd9208d60f
f-i <cfu3@student.kent.edu>
2022-05-24 15:27:35 -0400

    indenting edits

--- Algorithm_prioritizing_mutation_headerTop_updown.pl
+++ Algorithm_prioritizing_mutation_headerTop_updown.pl
@@ -18,120 +18,120 @@ open INFILE01, "$inputfile1";
 while (my $line01 = <INFILE01>) {
   chomp($line01);
   my @line01_array = split('\t', $line01);
-  if ($line01_array[0] eq "Sample")
+  if($line01_array[0] eq "Sample")
   {
     print OUTFILE01 $line01, "\n";
   }
   else
   {
-  $rowHash{$i} = $line01;
-  #get rid of right reciprocal pairs
-  if($line01_array[7] eq "SNP")
-  {
-    if($line01_array[12] eq "CDSHIT")
+    $rowHash{$i} = $line01;
+    #get rid of right reciprocal pairs
+    if($line01_array[7] eq "SNP")
     {
-      if($line01_array[9] =~ m/$comma/)
+      if($line01_array[12] eq "CDSHIT")
       {
-        my @snp_chars = split(/$comma/, $line01_array[9]);
-        #$case_counter = $#snp_chars + 1;
-        for(my $k=0; $k<($#snp_chars + 1); $k++)
+        if($line01_array[9] =~ m/$comma/)
         {
-          if($snp_chars[$k] eq "NSM")
+          my @snp_chars = split(/$comma/, $line01_array[9]);
+          #$case_counter = $#snp_chars + 1;
+          for(my $k=0; $k<($#snp_chars + 1); $k++)
+          {
+            if($snp_chars[$k] eq "NSM")
+            {
+              $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 6;
+            }
+            if($snp_chars[$k] eq "NSN")
+            {
+              $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 5;
+            }
+            if($snp_chars[$k] eq "SYN")
+            {
+              $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 4;
+            }
+          }
+          $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+        }
+        else
+        {
+          if($line01_array[9] eq "NSM")
           {
             $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 6;
           }
-          if($snp_chars[$k] eq "NSN")
+          if($line01_array[9] eq "NSN")
           {
             $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 5;
           }
-          if($snp_chars[$k] eq "SYN")
+          if($line01_array[9] eq "SYN")
           {
             $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 4;
           }
+          $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
         }
+      }
+      elsif($line01_array[12] eq "UPSTREAMHIT")
+      {
+        #use combination key: chr_SNPLocation_geneSymbol_rowNumber
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "DOWNSTREAMHIT")
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 2;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "UTR5HIT")
+      {
+        #use combination key: chr_SNPLocation_geneSymbol_rowNumber
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 1;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "UTR3HIT")
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 0;
         $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
       }
       else
       {
-        if($line01_array[9] eq "NSM")
-        {
-          $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 6;
-        }
-        if($line01_array[9] eq "NSN")
-        {
-          $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 5;
-        }
-        if($line01_array[9] eq "SYN")
-        {
-          $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 4;
-        }
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = -1;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+    }#if
+    else #INDEL case
+    {
+      if($line01_array[12] eq "CDSHIT")
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3.5;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "UPSTREAMHIT")
+      {
+        #use combination key: chr_SNPLocation_geneSymbol_rowNumber
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "DOWNSTREAMHIT")
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 2;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "UTR5HIT")
+      {
+        #use combination key: chr_SNPLocation_geneSymbol_rowNumber
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 1;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      elsif($line01_array[12] eq "UTR3HIT")
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 0;
+        $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
+      }
+      else
+      {
+        $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = -1;
         $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
       }
     }
-    elsif($line01_array[12] eq "UPSTREAMHIT")
-    {
-      #use combination key: chr_SNPLocation_geneSymbol_rowNumber
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "DOWNSTREAMHIT")
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 2;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "UTR5HIT")
-    {
-      #use combination key: chr_SNPLocation_geneSymbol_rowNumber
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 1;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "UTR3HIT")
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 0;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    else
-     {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = -1;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-  }#if
-  else #INDEL case
-  {
-    if($line01_array[12] eq "CDSHIT")
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3.5;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "UPSTREAMHIT")
-    {
-      #use combination key: chr_SNPLocation_geneSymbol_rowNumber
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 3;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "DOWNSTREAMHIT")
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 2;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "UTR5HIT")
-    {
-      #use combination key: chr_SNPLocation_geneSymbol_rowNumber
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 1;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    elsif($line01_array[12] eq "UTR3HIT")
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = 0;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-    else
-    {
-      $scoreHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = -1;
-      $infoHash{$line01_array[1]."+".$line01_array[2]."+".$line01_array[3]."+".$i} = [@line01_array];
-    }
-  }
-  $i++;
+    $i++;
   }
 }
 close(INFILE01);
@@ -146,7 +146,7 @@ for my $key21 (keys(%infoHash))
   for my $key22 (keys(%infoHash))
   {
     my @key22_array = split(/\+/, $key22);
-    if (($key21_array[0] eq $key22_array[0]) && ($key21_array[1] eq $key22_array[1]) && ($key21_array[2] eq $key22_array[2]))
+    if(($key21_array[0] eq $key22_array[0]) && ($key21_array[1] eq $key22_array[1]) && ($key21_array[2] eq $key22_array[2]))
     {
       if($scoreHash{$key22_array[0]."+".$key22_array[1]."+".$key22_array[2]."+".$key22_array[3]} > $scoreHash{$key21_array[0]."+".$key21_array[1]."+".$key21_array[2]."+".$key21_array[3]})
       {
@@ -163,6 +163,3 @@ for my $keyOut (keys(%outputHash))
 }
 
 close(OUTFILE01);
-
-
-
